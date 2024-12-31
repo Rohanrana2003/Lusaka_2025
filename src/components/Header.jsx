124e9787a07d4a3a92c986f30a755c9a1b99a50d
Rohan rana <rohanrana33042@gmail.com>
2024-12-31 13:39:42 +0530

    Formated the code

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import { useRef } from "react"
-import { BarIcon } from "../utils/constants"
+import { barIcon, crossIcon, facebookIcon, linkedinIcon, xIcon } from "../utils/constants"
 
 const Header = () =>{
 
@@ -23,19 +23,13 @@ const Header = () =>{
 
         <div className="space-x-4 md:absolute md:right-10 flex items-center max-md:ml-auto">
           <div className="w-7 h-7 flex items-center justify-center rounded-md text-gray-800 hover:bg-blue-600 hover:text-white transition-all">
-            <svg xmlns="http://www.w3.org/2000/svg" className="cursor-pointer w-5 h-5 fill-current transition-all" viewBox="0 0 155.139 155.139">
-              <path d="M89.584 155.139V84.378h23.742l3.562-27.585H89.584V39.184c0-7.984 2.208-13.425 13.67-13.425l14.595-.006V1.08C115.325.752 106.661 0 96.577 0 75.52 0 61.104 12.853 61.104 36.452v20.341H37.29v27.585h23.814v70.761h28.48z" data-original="#010002" />
-            </svg>
+            {facebookIcon}
           </div>
           <div className="w-7 h-7 flex items-center justify-center rounded-md text-gray-800 hover:bg-blue-600 hover:text-white transition-all">
-            <svg xmlns="http://www.w3.org/2000/svg" className="cursor-pointer w-5 h-5 fill-current transition-all" viewBox="0 0 24 24">
-              <path d="M23.994 24v-.001H24v-8.802c0-4.306-.927-7.623-5.961-7.623-2.42 0-4.044 1.328-4.707 2.587h-.07V7.976H8.489v16.023h4.97v-7.934c0-2.089.396-4.109 2.983-4.109 2.549 0 2.587 2.384 2.587 4.243V24zM.396 7.977h4.976V24H.396zM2.882 0C1.291 0 0 1.291 0 2.882s1.291 2.909 2.882 2.909 2.882-1.318 2.882-2.909A2.884 2.884 0 0 0 2.882 0z" data-original="#000000" />
-            </svg>
+            {linkedinIcon}
           </div>
           <div className="w-7 h-7 flex items-center justify-center rounded-md text-gray-800 hover:bg-black hover:text-white transition-all">
-            <svg xmlns="http://www.w3.org/2000/svg" className="cursor-pointer w-5 h-5 fill-current transition-all" viewBox="0 0 1226.37 1226.37">
-              <path d="M727.348 519.284 1174.075 0h-105.86L680.322 450.887 370.513 0H13.185l468.492 681.821L13.185 1226.37h105.866l409.625-476.152 327.181 476.152h357.328L727.322 519.284zM582.35 687.828l-47.468-67.894-377.686-540.24H319.8l304.797 435.991 47.468 67.894 396.2 566.721H905.661L582.35 687.854z" data-original="#000000" />
-            </svg>
+            {xIcon}
           </div>
         </div>
       </section>
@@ -45,14 +39,7 @@ const Header = () =>{
         <div id="collapseMenu" ref={collapseMenu}
           className='w-full max-lg:hidden lg:!block max-lg:before:fixed max-lg:before:bg-black max-lg:before:opacity-50 max-lg:before:inset-0 max-lg:before:z-50'>
           <button id="toggleClose" onClick={handleToggleHeader} className='lg:hidden fixed top-2 right-4 z-[100] rounded-full bg-white w-9 h-9 flex items-center justify-center border'>
-            <svg xmlns="http://www.w3.org/2000/svg" className="w-3.5 h-3.5 fill-black" viewBox="0 0 320.591 320.591">
-              <path
-                d="M30.391 318.583a30.37 30.37 0 0 1-21.56-7.288c-11.774-11.844-11.774-30.973 0-42.817L266.643 10.665c12.246-11.459 31.462-10.822 42.921 1.424 10.362 11.074 10.966 28.095 1.414 39.875L51.647 311.295a30.366 30.366 0 0 1-21.256 7.288z"
-                data-original="#000000"></path>
-              <path
-                d="M287.9 318.583a30.37 30.37 0 0 1-21.257-8.806L8.83 51.963C-2.078 39.225-.595 20.055 12.143 9.146c11.369-9.736 28.136-9.736 39.504 0l259.331 257.813c12.243 11.462 12.876 30.679 1.414 42.922-.456.487-.927.958-1.414 1.414a30.368 30.368 0 0 1-23.078 7.288z"
-                data-original="#000000"></path>
-            </svg>
+            {crossIcon}
           </button>
 
           <ul
@@ -88,7 +75,7 @@ const Header = () =>{
 
         <div className='flex ml-auto lg:hidden'>
           <button id="toggleOpen" onClick={handleToggleHeader}>
-            {BarIcon}
+            {barIcon}
           </button>
         </div>
       </div>
--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,5 +1,26 @@
-export const BarIcon = <svg className="w-7 h-7" fill="#000" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-<path fillRule="evenodd"
-  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-  clipRule="evenodd"></path>
+export const barIcon = <svg className="w-7 h-7" fill="#000" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+  <path fillRule="evenodd"
+    d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+    clipRule="evenodd"></path>
+</svg>
+
+export const crossIcon = <svg xmlns="http://www.w3.org/2000/svg" className="w-3.5 h-3.5 fill-black" viewBox="0 0 320.591 320.591">
+  <path
+    d="M30.391 318.583a30.37 30.37 0 0 1-21.56-7.288c-11.774-11.844-11.774-30.973 0-42.817L266.643 10.665c12.246-11.459 31.462-10.822 42.921 1.424 10.362 11.074 10.966 28.095 1.414 39.875L51.647 311.295a30.366 30.366 0 0 1-21.256 7.288z"
+    data-original="#000000"></path>
+  <path
+    d="M287.9 318.583a30.37 30.37 0 0 1-21.257-8.806L8.83 51.963C-2.078 39.225-.595 20.055 12.143 9.146c11.369-9.736 28.136-9.736 39.504 0l259.331 257.813c12.243 11.462 12.876 30.679 1.414 42.922-.456.487-.927.958-1.414 1.414a30.368 30.368 0 0 1-23.078 7.288z"
+    data-original="#000000"></path>
+</svg>
+
+export const facebookIcon = <svg xmlns="http://www.w3.org/2000/svg" className="cursor-pointer w-5 h-5 fill-current transition-all" viewBox="0 0 155.139 155.139">
+<path d="M89.584 155.139V84.378h23.742l3.562-27.585H89.584V39.184c0-7.984 2.208-13.425 13.67-13.425l14.595-.006V1.08C115.325.752 106.661 0 96.577 0 75.52 0 61.104 12.853 61.104 36.452v20.341H37.29v27.585h23.814v70.761h28.48z" data-original="#010002" />
+</svg>
+
+export const linkedinIcon = <svg xmlns="http://www.w3.org/2000/svg" className="cursor-pointer w-5 h-5 fill-current transition-all" viewBox="0 0 24 24">
+<path d="M23.994 24v-.001H24v-8.802c0-4.306-.927-7.623-5.961-7.623-2.42 0-4.044 1.328-4.707 2.587h-.07V7.976H8.489v16.023h4.97v-7.934c0-2.089.396-4.109 2.983-4.109 2.549 0 2.587 2.384 2.587 4.243V24zM.396 7.977h4.976V24H.396zM2.882 0C1.291 0 0 1.291 0 2.882s1.291 2.909 2.882 2.909 2.882-1.318 2.882-2.909A2.884 2.884 0 0 0 2.882 0z" data-original="#000000" />
+</svg>
+
+export const xIcon = <svg xmlns="http://www.w3.org/2000/svg" className="cursor-pointer w-5 h-5 fill-current transition-all" viewBox="0 0 1226.37 1226.37">
+<path d="M727.348 519.284 1174.075 0h-105.86L680.322 450.887 370.513 0H13.185l468.492 681.821L13.185 1226.37h105.866l409.625-476.152 327.181 476.152h357.328L727.322 519.284zM582.35 687.828l-47.468-67.894-377.686-540.24H319.8l304.797 435.991 47.468 67.894 396.2 566.721H905.661L582.35 687.854z" data-original="#000000" />
 </svg>
